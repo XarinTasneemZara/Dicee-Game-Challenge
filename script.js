1c75d2ae99312d4696d268faae9123b4379c2f45
Xarin Tasneem Zara <126411830+XarinTasneemZara@users.noreply.github.com>
2024-01-05 18:39:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor((Math.random())*6+1);
 var randomNumber2 = Math.floor((Math.random())*6+1);
-var randomDiceImg1 = "images/"+"dice"+randomNumber1+".png";
-var randomDiceImg2 = "images/"+"dice"+randomNumber2+".png";
+var randomDiceImg1 = "dice"+randomNumber1+".png";
+var randomDiceImg2 = "dice"+randomNumber2+".png";
 document.querySelectorAll("img")[0].setAttribute("src" , randomDiceImg1)
 document.querySelectorAll("img")[1].setAttribute("src" , randomDiceImg2)
 
